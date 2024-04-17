6aff1f66d88eb2caba97d9ef5c238b6eef42efec
Yuanfu Tian <yuanfu@sheridancollege.ca>
2024-04-17 04:49:47 -0400

    Modified footer.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -43,12 +43,6 @@ function Footer() {
         <Divider />
 
         {/* Site Map */}
-        <div className="container text-white py-16">
-          <>
-            <></>
-          </>
-        </div>
-        <Divider />
         <div className="text-white p-10">
           <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8">
             <div>
